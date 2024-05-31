c9c0867a89f0eb75da95662c1a7eb2f23092d236
Mateus Vieira <mateus.sly.mp@gmail.com>
2024-05-31 13:14:09 -0300

    fix: adjust for enviroment variable

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apikey: '620708fd9083b5396d7bb32077a8426a',    
+    apiURL: {
+        movies:'https://api.themoviedb.org/3/discover/movie',
+        genres:'https://api.themoviedb.org/3/genre/movie/list',
+        languages: 'https://api.themoviedb.org/3/configuration/languages'
+    }
+};
