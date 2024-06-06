1a22d36fa047662ef5475fc67c1fff48b94ea1ed
Isabela de Ponte <isabelapnt01@gmail.com>
2024-06-06 16:55:02 -0300

    feat: movie details

--- src/app/filmes/details/details.component.ts
+++ src/app/filmes/details/details.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { MaterialModule } from 'src/app/core/shared/material/material.module';
 import { Categoria } from 'src/app/entities/Categoria';
 import { Filme } from 'src/app/entities/Filme';
@@ -24,44 +24,16 @@ export class DetailsComponent {
   public secoes: Secao[] = [];
  
 
+  constructor(private secaoService: SecaoService, private cineService: CineService){}
 
-  constructor(private service: CineService, private SecaoService: SecaoService){}
 
   ngOnInit(){
-    this.filme = this.service.getDetails();
-    this.filme = {
-      "id": 653346,
-      "linguagem": {
-          "id": "en",
-          "nome": "English"
-      },
-      "titulo": "Kingdom of the Planet of the Apes",
-      "descricao": "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
-      "anoLancamento": 2024,
-      "dataLancamento": "2024-05-08",
-      "valorIngresso": 40,
-      "duracao": 71,
-      "avaliacao": 6.995,
-      "poster": "/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
-      "categorias": [
-          {
-              "id": 878,
-              "nome": "Science Fiction"
-          },
-          {
-              "id": 12,
-              "nome": "Adventure"
-          },
-          {
-              "id": 28,
-              "nome": "Action"
-          }
-      ]
-  }
-    this.imgUrl =`https://image.tmdb.org/t/p/w500/${this.filme.poster}`
-    this.dataEmCartaz = this.service.getDatasEmCartazString(this.filme.dataLancamento);
+    this.filme = this.cineService.getDetails();
+    this.secoes = this.secaoService.secoes.filter((secao: Secao) => secao.getFilme() == this.filme);
+    this.dataEmCartaz = this.cineService.getDatasEmCartazString(this.filme.dataLancamento);
     this.categorias = this.filme.categorias;
-    this.secoes = this.SecaoService.secoes.filter((secao: Secao) => secao.getFilme() == this.filme)
-  }
 
+    this.imgUrl = `https://image.tmdb.org/t/p/w500/${this.filme.poster}`;
+    
+  }
 }
